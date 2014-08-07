da89c60fc0d8577591c7beb30c97d66d89d8567a
Henry Lomas A <hlomas@celmedia.com>
2014-08-07 10:18:01 -0500

    Carrera CN cambios de isotipos

--- trabajaencn.php
+++ trabajaencn.php
@@ -17,7 +17,6 @@
 margin-bottom: 0px;
       }
       </style>
-      <!-- <div id="inicio" class="salto52"></div> -->
       <div class="row-fluid parallax-seccion-half" id="contribucion-ecuador" style="background-image: url('asset/img/banners/contactos/Banner-Trabaja-con-nosotros-2.jpg'); background-position: 50% 50%; ">
          <div class="span12 text-center contenedor-centrar">
             <h1 class="seccion-titular texto-blanco">Carrera CN</h1>
@@ -29,10 +28,6 @@ margin-bottom: 0px;
                <a href="https://careers.group.sabmiller.com/sap/bc/webdynpro/sap/hrrcf_a_unreg_job_search?sap-client=800&sap-wd-configId=%2fBGT%2fER_AC_UNR_JOB_SEARCH_HJ&sap-language=ES" class="proyecto-contenido5 text-center">
                   <div class="row-fluid height100">
                      <div class="span10 text-center offset1 contenedor-centrar">
-                       <!--  <h3 class="acceso-titulo">
-                           Busca posiciones vacantes en cualquier de nuestras operaciones alrededor del mundo.
-                        </h3> -->
-                        <!-- <hr class="acceso-divisor" /> -->
                         <div class="acceso-texto subtitulo mascara">
                            Busca posiciones vacantes en cualquier de nuestras operaciones alrededor del mundo.
                         </div>
@@ -42,17 +37,12 @@ margin-bottom: 0px;
                      </div>
                   </div>
                </a>
-               <!-- <img class="imgBloque5" src="asset/img/proyectos/desarrollo/1-programas.jpg" > -->
-               <div class="contenedor-centrar text-center subtitulo negrita mayuscula">Posiciones vacantes en el grupo Sabmiller</div>
+               <div class="contenedor-centrar text-center subtitulo negrita mayuscula"><div><img class="img-responsive1" src="asset/img/trabaja/icon-cn-vacantes.png"></div><div> Posiciones vacantes en el grupo Sabmiller</div></div>
             </div>
             <div  class="bloque5 proyecto-block5 proyecto1" style="height: 100%;" >
                <a href="https://careers.group.sabmiller.com/sap/bc/webdynpro/sap/hrrcf_a_refcode_srch?sap-client=800&sap-language=ES" class="proyecto-contenido5 text-center">
                   <div class="row-fluid height100">
                      <div class="span10 text-center offset1 contenedor-centrar">
-                        <!-- <h3 class="acceso-titulo">
-                           CONTRIBUCIÓN AL ECUADOR
-                        </h3> -->
-                        <!-- <hr class="acceso-divisor" /> -->
                         <div class="acceso-texto subtitulo mascara">
                            Ingresa aqu&#237; si conoces un c&#243;digo de referencia de la vacante que desea aplicar.
                         </div>
@@ -62,17 +52,12 @@ margin-bottom: 0px;
                      </div>
                   </div>
                </a>
-               <!-- <img class="imgBloque5" src="asset/img/proyectos/desarrollo/2-programas.jpg" > -->
-               <div class="contenedor-centrar text-center subtitulo negrita mayuscula">B&#250;squeda de empleo por c&#243;digo de referencia</div>
+               <div class="contenedor-centrar text-center subtitulo negrita mayuscula"><div><img class="img-responsive1" src="asset/img/trabaja/icono-cn-buscador-de-empleo.png"></div><div>B&#250;squeda de empleo por c&#243;digo de referencia</div></div>
             </div>
             <div class="bloque5 proyecto-block5 proyecto1" style="height: 100%;" >
                <a href="https://careers.group.sabmiller.com/sap/bc/webdynpro/sap/hrrcf_a_startpage_ext_cand?sap-client=800&sap-wd-configId=%2fBGT%2fER_AC_START_EXT&sap-language=ES" class="proyecto-contenido5 text-center">
                   <div class="row-fluid height100">
                      <div class="span10 text-center offset1 height100">
-                        <!-- <h3 class="acceso-titulo">
-                           PROCESOS AMIGABLES CON EL AMBIENTE
-                        </h3> -->
-                        <!-- <hr class="acceso-divisor" /> -->
                         <div class="acceso-texto subtitulo contenedor-centrar mascara">
                            Si ya tienes tu clave y contraseña, por favor ingrésala.
                         </div>
@@ -82,17 +67,12 @@ margin-bottom: 0px;
                      </div>
                   </div>
                </a>
-               <!-- <img class="imgBloque5" src="asset/img/proyectos/desarrollo/3-programas.jpg" > -->
-               <div class="contenedor-centrar text-center subtitulo negrita mayuscula">Iniciar sesión</div>
+               <div class="contenedor-centrar text-center subtitulo negrita mayuscula"><div><img class="img-responsive1" src="asset/img/trabaja/icono-cn-iniciar-.png"></div><div>Iniciar sesión</div></div>
             </div>
             <div  class="bloque5 proyecto-block5 proyecto1" style="height: 100%;" >
                <a href="https://careers.group.sabmiller.com/sap/bc/webdynpro/sap/hrrcf_a_candidate_registration?return-url=%2fsap%2fbc%2fwebdynpro%2fsap%2fhrrcf_a_startpage_ext_cand%3fsap-wd-configId%3d%252fBGT%252fER_AC_START_EXT#" class="proyecto-contenido5 text-center">
                   <div class="row-fluid height100">
                      <div class="span10 text-center offset1 contenedor-centrar">
-                        <!-- <h3 class="acceso-titulo">
-                           CONSUMO RESPONSABLE
-                        </h3> -->
-                        <!-- <hr class="acceso-divisor" /> -->
                         <div class="acceso-texto subtitulo mascara">
                            Si eres nuevo, crea una cuenta aqu&#237;.
                         </div>
@@ -102,17 +82,12 @@ margin-bottom: 0px;
                      </div>
                   </div>
                </a>
-               <!-- <img class="imgBloque5" src="asset/img/proyectos/desarrollo/4-programas.jpg" > -->
-               <div class="contenedor-centrar text-center subtitulo negrita mayuscula">Registrate</div>
+               <div class="contenedor-centrar text-center subtitulo negrita mayuscula"><div><img class="img-responsive1" src="asset/img/trabaja/icon-cn-registro.png"></div><div>Registrate</div></div>
             </div>
             <div  class="bloque5 proyecto-block5 proyecto1" style="height: 100%;" >
                <a href="#politicasModal" data-toggle="modal" class="proyecto-contenido5 text-center">
                   <div class="row-fluid height100">
                      <div class="span10 text-center offset1 contenedor-centrar">
-                        <!-- <h3 class="acceso-titulo">
-                           CERVEZA Y BIENESTAR
-                        </h3> -->
-                        <!-- <hr class="acceso-divisor" /> -->
                         <div class="acceso-texto subtitulo mascara">
                            Ingresa con tu clave y encuentra informaci&#243;n necesaria para tu nueva posici&#243;n.
                         </div>
@@ -122,8 +97,7 @@ margin-bottom: 0px;
                      </div>
                   </div>
                </a>
-               <!-- <img class="imgBloque5" src="asset/img/proyectos/desarrollo/5-programas.jpg" > -->
-               <div class="contenedor-centrar text-center subtitulo negrita mayuscula">Pol&#237;ticas internas</div>
+               <div class="contenedor-centrar text-center subtitulo negrita mayuscula"><div><img class="img-responsive1" src="asset/img/trabaja/icon-cn-politicas.png"></div><div>Pol&#237;ticas internas</div></div>
             </div>
          </div>
       </div>
@@ -205,93 +179,7 @@ margin-bottom: 0px;
                      </a>
                   </div>
                </div>
-               <!-- <div class="span2 fichacontacto ultimo">
-                  <div class="acceso">
-                     <img src="asset/img/contactos/7-Visita-Planta.jpg" class="" alt="">
-                     <a href="contactenos.php" class="caption-accesocontacto" >
-                        <div class="rojo">
-                            
-                           <div class=" texto-acceso2 movil">
-                              Planificar una visita a la planta
-                           </div>
-                            
-                           <div class="iconocontacto movil">
-                             <img src="asset/img/Icono-Mas.png" alt="">
-                           </div>
-                        </div>
-                     </a>
-                  </div>
-                  </div> -->
             </div>
-      <!-- <div class="row-fluid fichas visible-desktop visible-tablet">
-         <div class="acceso flotadores" style="display:inline-block;height:100%;">
-            <img src="asset/img/contactos/1-Icono-Clientes-nuevos.jpg" class="" alt="">
-            <div class="contenedor-centrar" style="width:274px;height:307px;">Busca posiciones vacantes en cualquier de nuestras operaciones alrededor del mundo.</div>
-            <a href="contactenos.php" class="caption-accesocontacto" >
-               <div class="rojo">
-                  <div class=" texto-acceso2">
-                     Desea comprar/vender nuestros productos.
-                  </div>
-                  <div class="iconocontacto">
-                     <img src="asset/img/Icono-Mas.png" alt="">
-                  </div>
-               </div>
-            </a>
-         </div>
-         <div class="acceso flotadores">
-            <img src="asset/img/contactos/2-Icono-Soluciones.jpg" class="" alt="">
-            <a href="contactenos.php" class="caption-accesocontacto" >
-               <div class="rojo">
-                  <div class=" texto-acceso2">
-                     Problemas con la entrega/mantenimiento/toma de pedidos
-                  </div>
-                  <div class="iconocontacto">
-                     <img src="asset/img/Icono-Mas.png" alt="">
-                  </div>
-               </div>
-            </a>
-         </div>
-         <div class="acceso flotadores">
-            <img src="asset/img/contactos/3-Trabajadores.jpg" class="" alt="">
-            <a href="contactenos.php" class="caption-accesocontacto" >
-               <div class="rojo">
-                  <div class=" texto-acceso2">
-                     Quieres trabajar en CN
-                  </div>
-                  <div class="iconocontacto">
-                     <img src="asset/img/Icono-Mas.png" alt="">
-                  </div>
-               </div>
-            </a>
-         </div>
-         <div class="acceso flotadores">
-            <img src="asset/img/contactos/7-Visita-Planta.jpg" class="" alt="">
-            <a href="visita.php" class="caption-accesocontacto" >
-               <div class="rojo">
-                  <div class=" texto-acceso2">
-                     Planificar una visita a la planta
-                  </div>
-                  <div class="iconocontacto">
-                     <img src="asset/img/Icono-Mas.png" alt="">
-                  </div>
-               </div>
-            </a>
-         </div>
-         <div class="acceso flotadores ultimo">
-            <img src="asset/img/contactos/6-Proveedores.jpg" class="" alt="">
-            <a href="contactenos.php" class="caption-accesocontacto" >
-               <div class="rojo">
-                  <div class=" texto-acceso2">
-                     Desea proveer nuestros productos
-                  </div>
-                  <div class="iconocontacto">
-                     <img src="asset/img/Icono-Mas.png" alt="">
-                  </div>
-               </div>
-            </a>
-         </div>
-      </div> -->
-      <!-- Modal -->
       <div id="politicasModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="politicaslLabel" aria-hidden="true">
          <div class="modal-body">
             <div class="row-fluid text-center">
@@ -305,12 +193,6 @@ margin-bottom: 0px;
                <div class="span12">
                   <input type="password" name="miclave" id="miclave" class="" >
                </div>
-               <!-- <div class="span4">
-                  <input type="text" name="nac-mes" id="nac-mes" class="input-nac integer" placeholder="MM" maxlength="2" size="2">
-               </div>
-               <div class="span4">
-                  <input type="text" name="nac-anio" id="nac-anio" class="input-nac integer" placeholder="AA" maxlength="2" size="2">
-               </div> -->
             </div>
          </div>
          <div class="modal-footer">
@@ -334,8 +216,6 @@ margin-bottom: 0px;
     var ancho_aplicar = (ancho / 5 )-6;
     var ancho_aplicar = ancho_aplicar - 1;
     $(".flotadores").css("width",   ancho_aplicar + "px");
-    // centrarVert($(".texto-acceso2"));
-    // centrarVert($(".texto-acceso2.movil"));
     centrarHor($(".iconocontacto"));
     centrarHor($(".iconocontacto.movil"));
 
@@ -349,43 +229,19 @@ margin-bottom: 0px;
 
   });
 
-  
-//   function centrarVert(elemento){
-//    altura1=elemento.parent().height();
-//    altura2=elemento.height();
-//    if (altura1>=altura2) {
-//     elemento.css("margin-top",(altura1/2)-(altura2/2)+"px");
+  function ajustar(){
 
-//   }else{
-//     elemento.css("margin-top","20px");
-//   };
-  
 
-// }
-// function centrarHor(elemento){
-//  base1=elemento.parent().width();
-//  base2=elemento.width();        
-//  elemento.css("margin-left",(base1/2)-(base2/2)+"px");
-
-// }
-     function ajustar(){
-         
-         
-          var ancho = $(window).width(); 
-          var ancho_aplicar = ancho / 5 ;
-          // var ancho_aplicar = ancho_aplicar - 0.5;
-                    //$(".flotadores").css("width",   ancho_aplicar + "px");
-                    $(".flotadores").width(ancho_aplicar + "px")
-                    $(".flotadores").height($(".flotadores").width());
-                    $(".flotadores").height($(".flotadores").width());
-         
-                    //$('.imagenfull').each(centrarVert($(this)));
-                    centrarVert($(".imagenfull"));
-                    $('.texto-acceso2').each(function () {
-                      centrarVert($(this));
-                    });
-                     //centrarVert($(".texto-acceso2"));
-                   }
+     var ancho = $(window).width(); 
+     var ancho_aplicar = ancho / 5 ;
+     $(".flotadores").width(ancho_aplicar + "px");
+     $(".flotadores").height($(".flotadores").width());
+     $(".flotadores").height($(".flotadores").width());
+     centrarVert($(".imagenfull"));
+     $('.texto-acceso2').each(function () {
+        centrarVert($(this));
+     });
+  }
 $(document).ready(function(){
 
 
